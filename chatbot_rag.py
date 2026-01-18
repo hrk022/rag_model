e5bb7faee277488aa6bcee9a55a480713eca4645
hrk022 <154806834+hrk022@users.noreply.github.com>
2026-01-18 18:07:03 +0530

    Refactor chatbot_rag.py for improved structure

--- chatbot_rag.py
+++ chatbot_rag.py
@@ -9,7 +9,8 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
 from langchain_openai import ChatOpenAI
-from langchain.memory import ConversationBufferMemory  
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
 from langchain.callbacks.base import BaseCallbackHandler
 
 
